dae1bb01ff2ce50438d136250e97e81052b35920
rotichknovestus <noverotich@gmail.com>
2023-10-17 10:43:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// Variables - These are named memory location for data
+    	// Tpo create a variable in javascript we use the keyword let... Example:
+
+
+        let message; // this statement creats a variable with the name message
+        message = "Hello world"; //the message variable stores the string 'hello worlld'
+        alert(message);
+
+        let fName = 'Joe', age = 32, msg = 'Hello'
+
+        //Rules for declaring variables;
+            //1. Always start with the key word
+            //2. Never declare two variables using the same name
+            // The name must contain only letters, digits or the symbols $ and _
+            //3. Always start with alphabet characters
+            //4. Don't use numbers to name ur variables
+            //5. After declaring a variables don't 4get to terminate(;)
+            //6. After initializing a variable don't 4get to terminate(;)
+            //7. When the name contains multiple words always use camelCase ie firstName
+            //8. Case matters name and Name are two diffrent variables. Always ensure to check on your casing
+            //9. Avoid using reserver or key word eg let
+            //10. If you want to have a variable that does not change, declare the variable using the keyword const ie cont pi = 3.142
+
+
+            //Summery
+            //We can declare variables to store data by using the var, let or const keywords.
+            //let - is a modern variable declaration
+            //var - is an old-school variable declaration. Normally we don't use it at all
+            //const - is like let but now the value of the variable can't be changed anymore
+            //Variables should be named in a way that allows us to easlly understand what's inside them.
+
+
+            //Task
+            //declare two variables: admin and name.
+            //Assign values to the declared variables
+            //reassign the value to both variables
+            //Show the values of the two variables using alert(must outlut the assigned values)
+
+
+            let admin = 'Josee';
+
+            let name = 'Novestus';
+
+            alert(admin);
+            alert(name);
+// Data types 
+// Control structures
