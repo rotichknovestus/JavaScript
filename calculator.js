4e81899fae050c6113ad7740fef21c86cd1d1602
rotichknovestus <noverotich@gmail.com>
2023-10-24 08:14:38 +0300

    added text

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let a = 3;
+let b = 3;
+results = a + b; //!adding 2 variables
+
+console.log(results);
+
+document.write("<h1>Results is: "+results+" </h1>");
+
+
+let isStudent = 'Novestues';
+
+console.log(isStudent);
+
+let age = '20';
+console.log(age)
+
+document.write("<h1>Results age is: "+age+" </h1>");
+
